b432fbb5d6917d76564cc2fcb4ae75e0b3398711
Troy Neubauer <troyneubauer@gmail.com>
2022-03-24 14:47:44 -0400

    Finished structs for assets, exchanges and historical data

--- src/lib.rs
+++ src/lib.rs
@@ -1,7 +1,9 @@
 //! Bindings to the [coinapi](https://www.coinapi.io/jq) cryptocurrency api
 //! Currently only the Market Data REST API is supported
-use std::time::{Duration, SystemTime};
+use chrono::{Date, DateTime, NaiveDate, Utc};
+use std::time::Duration;
 
+use serde::{Deserialize, Deserializer};
 use thiserror::Error;
 
 #[derive(Error, Debug)]
@@ -11,6 +13,9 @@ pub enum Error {
 
     #[error("reqwest {0}")]
     Reqwest(#[from] reqwest::Error),
+
+    #[error("json decode {0}")]
+    Json(#[from] serde_json::Error),
 }
 
 pub struct Coinapi {
@@ -34,6 +39,17 @@ pub enum PeriodInner {
 /// A supported period for which historical data can be obtained
 #[derive(Copy, Clone, Debug, PartialEq, Eq)]
 pub struct Period(PeriodInner);
+
+impl std::fmt::Display for Period {
+    fn fmt(&self, f: &mut std::fmt::Formatter<'_>) -> std::fmt::Result {
+        match self.0 {
+            PeriodInner::Second(s) => f.write_fmt(format_args!("{s}SEC")),
+            PeriodInner::Minute(m) => f.write_fmt(format_args!("{m}MIN")),
+            PeriodInner::Hour(h) => f.write_fmt(format_args!("{h}HRS")),
+            PeriodInner::Day(d) => f.write_fmt(format_args!("{d}DAY")),
+        }
+    }
+}
 
 /// Returned when the requested period is not supported by coinapi
 pub struct ExactError {
@@ -153,7 +169,170 @@ impl Period {
     }
 }
 
-pub struct TimeseriesData {}
+#[derive(Deserialize)]
+pub struct TimeseriesData(pub Vec<TimeseriesDatum>);
+
+#[derive(Deserialize)]
+pub struct TimeseriesDatum {
+    #[serde(deserialize_with = "de_date_time")]
+    pub time_period_start: DateTime<Utc>,
+    #[serde(deserialize_with = "de_date_time")]
+    pub time_period_end: DateTime<Utc>,
+    #[serde(deserialize_with = "de_date_time")]
+    pub time_open: DateTime<Utc>,
+    #[serde(deserialize_with = "de_date_time")]
+    pub time_close: DateTime<Utc>,
+
+    pub rate_open: f64,
+    pub rate_high: f64,
+    pub rate_low: f64,
+    pub rate_close: f64,
+}
+
+fn de_date_time<'de, D>(d: D) -> Result<DateTime<Utc>, D::Error>
+where
+    D: Deserializer<'de>,
+{
+    let s = String::deserialize(d)?;
+    let fixed = DateTime::parse_from_rfc3339(&s).map_err(serde::de::Error::custom)?;
+    Ok(fixed.with_timezone(&Utc))
+}
+
+fn de_date_time_option<'de, D>(d: D) -> Result<Option<DateTime<Utc>>, D::Error>
+where
+    D: Deserializer<'de>,
+{
+    Ok(match Option::<String>::deserialize(d)? {
+        Some(s) => {
+            let fixed = DateTime::parse_from_rfc3339(&s).map_err(serde::de::Error::custom)?;
+            Some(fixed.with_timezone(&Utc))
+        }
+        None => None,
+    })
+}
+
+#[derive(Deserialize)]
+pub struct Exchanges(pub Vec<Exchange>);
+
+#[derive(Deserialize)]
+pub struct Exchange {
+    pub exchange_id: String,
+    pub website: String,
+    pub name: String,
+
+    #[serde(default)]
+    #[serde(deserialize_with = "de_date_option")]
+    pub data_start: Option<NaiveDate>,
+    #[serde(default)]
+    #[serde(deserialize_with = "de_date_option")]
+    pub data_end: Option<NaiveDate>,
+
+    #[serde(default)]
+    #[serde(deserialize_with = "de_date_time_option")]
+    pub data_quote_start: Option<DateTime<Utc>>,
+    #[serde(default)]
+    #[serde(deserialize_with = "de_date_time_option")]
+    pub data_quote_end: Option<DateTime<Utc>>,
+
+    #[serde(default)]
+    #[serde(deserialize_with = "de_date_time_option")]
+    pub data_orderbook_start: Option<DateTime<Utc>>,
+    #[serde(default)]
+    #[serde(deserialize_with = "de_date_time_option")]
+    pub data_orderbook_end: Option<DateTime<Utc>>,
+    #[serde(default)]
+    #[serde(deserialize_with = "de_date_time_option")]
+    pub data_trade_start: Option<DateTime<Utc>>,
+    #[serde(default)]
+    #[serde(deserialize_with = "de_date_time_option")]
+    pub data_trade_end: Option<DateTime<Utc>>,
+
+    pub data_symbols_count: usize,
+    pub volume_1hrs_usd: f64,
+    pub volume_1day_usd: f64,
+    pub volume_1mth_usd: f64,
+}
+
+fn de_date_option<'de, D>(d: D) -> Result<Option<NaiveDate>, D::Error>
+where
+    D: Deserializer<'de>,
+{
+    Ok(match Option::<String>::deserialize(d)? {
+        Some(s) => {
+            dbg!(&s);
+            Some(NaiveDate::parse_from_str(&s, "%Y-%m-%d").map_err(serde::de::Error::custom)?)
+        }
+        None => {
+            println!("at none");
+            None
+        }
+    })
+}
+
+fn de_date<'de, D>(d: D) -> Result<NaiveDate, D::Error>
+where
+    D: Deserializer<'de>,
+{
+    let s = String::deserialize(d)?;
+    Ok(NaiveDate::parse_from_str(&s, "%Y-%m-%d").map_err(serde::de::Error::custom)?)
+}
+
+fn de_int_bool<'de, D>(d: D) -> Result<bool, D::Error>
+where
+    D: Deserializer<'de>,
+{
+    match u8::deserialize(d)? {
+        0 => Ok(false),
+        1 => Ok(true),
+        _ => Err(serde::de::Error::custom("")),
+    }
+}
+
+#[derive(Deserialize)]
+pub struct Assets(pub Vec<Asset>);
+
+#[derive(Deserialize)]
+pub struct Asset {
+    asset_id: String,
+    name: String,
+    #[serde(deserialize_with = "de_int_bool")]
+    type_is_crypto: bool,
+
+    #[serde(default)]
+    #[serde(deserialize_with = "de_date_option")]
+    data_start: Option<NaiveDate>,
+    #[serde(default)]
+    #[serde(deserialize_with = "de_date_option")]
+    data_end: Option<NaiveDate>,
+
+    #[serde(default)]
+    #[serde(deserialize_with = "de_date_time_option")]
+    data_quote_start: Option<DateTime<Utc>>,
+    #[serde(default)]
+    #[serde(deserialize_with = "de_date_time_option")]
+    data_quote_end: Option<DateTime<Utc>>,
+
+    #[serde(default)]
+    #[serde(deserialize_with = "de_date_time_option")]
+    data_orderbook_start: Option<DateTime<Utc>>,
+    #[serde(deserialize_with = "de_date_time_option")]
+    #[serde(default)]
+    data_orderbook_end: Option<DateTime<Utc>>,
+
+    #[serde(default)]
+    #[serde(deserialize_with = "de_date_time_option")]
+    data_trade_start: Option<DateTime<Utc>>,
+    #[serde(default)]
+    #[serde(deserialize_with = "de_date_time_option")]
+    data_trade_end: Option<DateTime<Utc>>,
+
+    data_symbols_count: usize,
+    volume_1hrs_usd: f64,
+    volume_1day_usd: f64,
+    volume_1mth_usd: f64,
+    #[serde(default)]
+    price_usd: Option<f64>,
+}
 
 impl Coinapi {
     /// Sends a get request to the server with api v1 at `route` with params as URL parameters
@@ -172,16 +351,37 @@ impl Coinapi {
         Ok(json)
     }
 
+    /// Queries the `exchangerate/{asset_id_base}/{asset_id_quote}/history` endpoint for historical
+    /// data for a security pair during a time interval
     pub async fn timeseries_data(
         &self,
         base: AssetName,
         quote: AssetName,
         period: Period,
-        start: SystemTime,
-        end: SystemTime,
+        start: DateTime<Utc>,
+        end: DateTime<Utc>,
         limit: usize,
     ) -> Result<TimeseriesData, Error> {
-        todo!()
+        let base = &base.0;
+        let quote = &quote.0;
+        let period = period.to_string();
+        let start = start.to_rfc3339();
+        let end = end.to_rfc3339();
+        let limit = limit.to_string();
+        let json = self
+            .get(
+                format!("exchangerate/{base}/{quote}/history"),
+                [
+                    ("period_id", period.as_str()),
+                    ("time_start", &start),
+                    ("time_end", &end),
+                    ("limit", &limit),
+                ]
+                .into_iter(),
+            )
+            .await?;
+
+        Ok(serde_json::from_str(&json)?)
     }
 }
 
@@ -234,5 +434,37 @@ mod tests {
             Duration::from_secs_f32(14.0 * 60.0 * 60.0 * 24.0),
             p(PeriodInner::Day(10)),
         );
+    }
+
+    #[test]
+    fn period_formattintg() {
+        let s = [
+            "1SEC", "2SEC", "3SEC", "4SEC", "5SEC", "6SEC", "10SEC", "15SEC", "20SEC", "30SEC",
+            "1MIN", "2MIN", "3MIN", "4MIN", "5MIN", "6MIN", "10MIN", "15MIN", "20MIN", "30MIN",
+            "1HRS", "2HRS", "3HRS", "4HRS", "6HRS", "8HRS", "12HRS", "1DAY", "2DAY", "3DAY",
+            "5DAY", "7DAY", "10DAY",
+        ];
+        assert_eq!(s.len(), SUPPORTED_PERIODS.len());
+        let real: Vec<_> = SUPPORTED_PERIODS.iter().map(|p| p.to_string()).collect();
+        for expected in s {
+            assert!(real.contains(&expected.to_owned()));
+        }
+    }
+
+    #[test]
+    fn xdb_history_format() {
+        let _: TimeseriesData =
+            serde_json::from_str(include_str!("../test_files/xdb_history.json")).unwrap();
+    }
+
+    #[test]
+    fn assets_format() {
+        let _: Assets = serde_json::from_str(include_str!("../test_files/assets.json")).unwrap();
+    }
+
+    #[test]
+    fn exchanges_format() {
+        let _: Exchanges =
+            serde_json::from_str(include_str!("../test_files/exchanges.json")).unwrap();
     }
 }
